9cec372281f4b30002934f1f49c4bf0440691f70
vale075 <63117317+vale075@users.noreply.github.com>
2026-03-15 21:07:42 +0100

    Correction de la mesure de la tension

--- lib/battery/battery.cpp
+++ lib/battery/battery.cpp
@@ -1,7 +1,9 @@
 #include "battery.h"
 
 const uint8_t battery_pin = 1;
-const double battery_k = 15.0 / 115 / 6; // 15k 100k resistor divider plus 6 cells
+const double R1 = 10.0;
+const double R2 = 100.0;
+const double battery_k = (R1 + R2) / R1 / 6; // 15k 100k resistor divider plus 6 cells
 const uint32_t min_voltage = 3500;       // millivolts
 bool battery_empty = false;
 
